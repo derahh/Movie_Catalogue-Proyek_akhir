c2ccef857db4426a913d92b04b49fbca68c4ad41
Dedi Rahmansah <derah666@gmail.com>
2019-10-01 20:10:05 +0700

    add menu in movie fragment

--- app/src/main/java/id/co/derahh/moviecatalogue/fragment/MovieFragment.java
+++ app/src/main/java/id/co/derahh/moviecatalogue/fragment/MovieFragment.java
@@ -1,14 +1,24 @@
 package id.co.derahh.moviecatalogue.fragment;
 
 
+import android.app.SearchManager;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
@@ -43,6 +53,10 @@ public class MovieFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_list, container, false);
 
+        super.onViewCreated(view, savedInstanceState);
+        final Toolbar toolbar = view.findViewById(R.id.toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+
         recyclerView = view.findViewById(R.id.list);
         progressBar = view.findViewById(R.id.progress_bar);
         tvNoData = view.findViewById(R.id.tv_no_data);
@@ -52,7 +66,39 @@ public class MovieFragment extends Fragment {
 
         addItem();
 
+        ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+        if (actionBar != null){
+            actionBar.setTitle(R.string.category_movie);
+        }
+
         return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_search, menu);
+
+
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_search){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private Observer<ArrayList<Movie>> getMovie = new Observer<ArrayList<Movie>>() {
