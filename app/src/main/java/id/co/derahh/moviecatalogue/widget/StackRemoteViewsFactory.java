89d8a3c69a7104edef2fce38a66d6bfcaade87f6
Dedi Rahmansah <derah666@gmail.com>
2019-10-06 06:55:41 +0700

    add remoteViewFactory

---
+++ app/src/main/java/id/co/derahh/moviecatalogue/widget/StackRemoteViewsFactory.java
@@ -0,0 +1,76 @@
+package id.co.derahh.moviecatalogue.widget;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import id.co.derahh.moviecatalogue.R;
+
+public class StackRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
+
+    private final Context mContext;
+
+    StackRemoteViewsFactory(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public void onCreate() {
+
+    }
+
+    @Override
+    public void onDataSetChanged() {
+    }
+
+    @Override
+    public void onDestroy() {
+
+    }
+
+    @Override
+    public int getCount() {
+        return mWidgetItems.size();
+    }
+
+    @Override
+    public RemoteViews getViewAt(int i) {
+        RemoteViews rv = new RemoteViews(mContext.getPackageName(), R.layout.widget_item);
+        rv.setImageViewBitmap(R.id.imageView, mWidgetItems.get(i));
+
+        Bundle extras = new Bundle();
+        extras.putInt(ImagesBannerWidget.EXTRA_ITEM, i);
+        Intent fillInIntent = new Intent();
+        fillInIntent.putExtras(extras);
+
+        rv.setOnClickFillInIntent(R.id.imageView, fillInIntent);
+        return rv;
+    }
+
+    @Override
+    public RemoteViews getLoadingView() {
+        return null;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 1;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+}
