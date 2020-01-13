b366ec540a41a9f52ebe871771f85393a8eb337f
Dedi Rahmansah <derah666@gmail.com>
2020-01-13 17:01:52 +0700

    Create MovieRoomDatabase.java

---
+++ moviecatalogueretrofit/src/main/java/com/derahh/moviecatalogueretrofit/database/MovieRoomDatabase.java
@@ -0,0 +1,31 @@
+package com.derahh.moviecatalogueretrofit.database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.derahh.moviecatalogueretrofit.model.Result;
+
+@Database(entities = {Result.class}, version = 1)
+public abstract class MovieRoomDatabase extends RoomDatabase {
+
+    public abstract MovieDao movieDao();
+
+    private static volatile MovieRoomDatabase mInstance;
+
+    static MovieRoomDatabase getDatabase(final Context context) {
+        if (mInstance == null) {
+            synchronized (MovieRoomDatabase.class) {
+                if (mInstance == null) {
+                    mInstance = Room.databaseBuilder(context.getApplicationContext(),
+                            MovieRoomDatabase.class, "movie_db")
+                            .allowMainThreadQueries()
+                            .build();
+                }
+            }
+        }
+        return mInstance;
+    }
+}
